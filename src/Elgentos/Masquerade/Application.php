30490e0dc8e140b20792254fabb85bdba7a29266
peterjaap <peterjaap@elgentos.nl>
2018-07-13 11:27:53 +0200

    Moved custom autoloader to custom Application class, changed order of path prefixes to look in and added break in foreach loop to prevent class already defined errors

---
+++ src/Elgentos/Masquerade/Application.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Elgentos\Masquerade;
+
+use Symfony\Component\Console\Application as SymfonyApplication;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Application extends SymfonyApplication
+{
+
+    public function run(InputInterface $input = null, OutputInterface $output = null)
+    {
+        $this->registerAutoloader();
+        return parent::run($input, $output);
+    }
+
+    private function registerAutoloader()
+    {
+        spl_autoload_register(function ($class) {
+            $class = str_replace('\\','/', $class);
+
+            $pathPrefixToLookForCustomFormatters = [
+                '',
+                __DIR__. '/',
+                $_SERVER['HOME'] . '/'
+            ];
+
+            foreach ($pathPrefixToLookForCustomFormatters as $path) {
+                $file = $path . '.masquerade/' . $class . '.php';
+                if (file_exists($file)) {
+                    require_once $file;
+                    break;
+                }
+            }
+        });
+    }
+
+}
