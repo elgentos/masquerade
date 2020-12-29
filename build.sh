d7e2f2ecefc1d0f734b8e1ef33dc4a93d64a2ece
John ORourke <john@getjohn.co.uk>
2020-12-29 20:58:36 +0000

    fixed build.sh to work with the default box installer process

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
+mkdir -p dist
 if [ -f ../box/box.phar ]; then
     php -d phar.readonly=off ../box/box.phar build -v
+elif [ -f ./box.phar ]; then
+    php -d phar.readonly=off ./box.phar build -v
 else
-    php -d phar.readyonly=off box build -v
+    php -d phar.readonly=off box build -v
 fi
