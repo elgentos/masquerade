9a260629cbb3bcd11d02238f56db6cafc348c855
John <john@getjohn.co.uk>
2021-01-04 18:12:55 +0000

    Tested and working with Magento 2.3.4, Amasty Order Attributes, and regular tables

--- src/Elgentos/Masquerade/Provider/Table/Magento2Eav.php
+++ src/Elgentos/Masquerade/Provider/Table/Magento2Eav.php
@@ -22,14 +22,24 @@ class Magento2Eav extends Simple
     public function setup()
     {
         // find all EAV attribues for this table:
-        $this->entity = $this->db->table('eav_entity_types')->where('entity_table', $this->table['name'])->first();
+        $this->entity = $this->db->table('eav_entity_type')->where('entity_table', $this->table['name'])->first();
         if (!$this->entity) {
             throw new \Exception("Table {$this->table['name']} is not associated with EAV entities");
         }
         $attributes = $this->db->table('eav_attribute')->where('entity_type_id', $this->entity->entity_type_id)->get();
         foreach ($attributes as $attribute) {
             $this->attributes[$attribute->attribute_code] = $attribute;
+            if ($attribute->is_unique && isset($this->table['columns']) && isset($this->table['columns'][$attribute->attribute_code])) {
+                $this->output->writeln(' - forcing ' . $attribute->attribute_code . ' to be unique');
+                $this->table['columns'][$attribute->attribute_code]['unique'] = true;
+            }
         }
+
+	// useful data in $this->attributes[$code]:
+        // ->attribute_id
+        // ->backend_type - gives us the table suffix
+        // ->is_unique - we use this to apply the 'unique' faker setting
+        // ->is_required - we don't use this, because if it's a required value then it'll already exist in the DB
         
         parent::setup();
     }
@@ -46,13 +56,37 @@ class Magento2Eav extends Simple
     {
 
         // first update the static properties:
-        $staticUpdates = array_filter($updates, function ($value, $key) {
-            return $this->attributes[$key]->backend_type === 'static';
+        $staticUpdates = array_filter($updates, function ($value, $code) {
+            return $this->_isInBaseTable($code);
         }, ARRAY_FILTER_USE_BOTH);
 
-        $this->db->table($this->table['name'])->where($this->table['pk'], $primaryKey)->update($updates);
+	// only update static values if there are any:
+        if (count($staticUpdates)) {
+            $this->db->table($this->table['name'])->where($this->table['pk'], $primaryKey)->update($staticUpdates);
+        }
 
-        // now individually update any EAV tables using $attribute->backend_type to determine table name...
+        // now individually update any EAV tables using $attribute->backend_type to determine table name:
+	// NOTE: for attributes with per-store values (eg. products) this will put the same value in for each store ID
+        foreach($updates as $code => $value) {
+            if ($this->_isInBaseTable($code)) {
+                continue;
+            }
+            // determine the table name for this attribute (we're ignoring eav_attribute->backend_table because nothing uses it)
+            $table = $this->table['name'] . '_' . $this->attributes[$code]->backend_type;
+            // we're assuming the entity ID field is 'entity_id' - could be overridden in eav_entity_type->entity_id_field but unlikely
+            // update anything for this entity ID
+            $this->db->table($table)
+		->where('entity_id', '=', $primaryKey)
+		->where('attribute_id', '=', $this->attributes[$code]->attribute_id)
+		->update([
+		    'value' => $value
+		]);
+        }
+    }
+
+    protected function _isInBaseTable($attributeCode)
+    {
+        return !isset($this->attributes[$attributeCode]) || $this->attributes[$attributeCode]->backend_type === 'static';
     }
 
     /**
@@ -74,7 +108,7 @@ class Magento2Eav extends Simple
                 continue;
             }
             $joinTable = $this->table['name'] . '_' . $attr->backend_type; // only for basic EAV, not things like tier_price
-            $query->leftJoin($joinTable, function ($join) use ($joinTable) {
+            $query->leftJoin($joinTable, function ($join) use ($joinTable, $attr) {
                 $join->on("{$this->table['name']}.{$this->table['pk']}", '=', "{$joinTable}.entity_id")
                     ->where("{$joinTable}.attribute_id", '=', $attr->attribute_id);
             });
