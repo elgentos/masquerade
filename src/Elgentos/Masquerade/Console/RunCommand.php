eb545ab8f65a89762c1c239f2b7932b94df57258
Peter Jaap Blaakmeer <peterjaap@elgentos.nl>
2021-02-05 15:23:51 +0100

    Raised version to 0.2.3

--- src/Elgentos/Masquerade/Console/RunCommand.php
+++ src/Elgentos/Masquerade/Console/RunCommand.php
@@ -21,7 +21,7 @@ class RunCommand extends Command
             |
                    by elgentos';
 
-    const VERSION = '0.2.2';
+    const VERSION = '0.2.3';
 
     const DEFAULT_QUERY_PROVIDER = \Elgentos\Masquerade\Provider\Table\Simple::class;
 
