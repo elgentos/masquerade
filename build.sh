b4e79ca0c7e6fb57d7f79f6b2134c9c397fc9560
Peter Jaap Blaakmeer <peterjaap@elgentos.nl>
2021-03-02 12:13:11 +0100

    Updated build command

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 mkdir -p dist
 if [ -f ../box/box.phar ]; then
-    php -d phar.readonly=off ../box/box.phar build -v
+    php -d phar.readonly=off ../box/box.phar compile -v
 elif [ -f ./box.phar ]; then
-    php -d phar.readonly=off ./box.phar build -v
+    php -d phar.readonly=off ./box.phar compile -v
 else
-    php -d phar.readonly=off box build -v
+    php -d phar.readonly=off box compile -v
 fi
